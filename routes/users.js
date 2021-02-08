62c0b16779da6e1a15faaf666f4bdedfaded1f2a
alexhauka <alexhauka@gmail.com>
2021-02-08 21:05:19 +0000

    add message route for privately messaging another user

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,9 @@ const {
   getUsers,
   getUserById,
   getFavorites,
-  getMessages
+  getMessages,
+  getMessage,
+  sendMessage
 } = require('../lib/user-queries');
 
 //get users
@@ -48,11 +50,53 @@ router.get('/:id/messages', (req, res) => {
     getMessages(userId)
     .then((results) => {
       // res.render('template')
+      // console.log(results)
       res.json(results)
-    });
+    })
+    .catch(e => {
+      console.error(e);
+      res.send(e);
+    })
   } else {
     res.redirect('/');
   }
 });
+
+//get /users/:id/messages/:messageid
+router.get('/:id/messages/:messageid', (req, res) => {
+  if (req.session.user_id) {
+    const userId = req.session.user_id;
+    const otherId = req.params.messageid;
+    getMessage(userId, otherId)
+    .then((results) => {
+      // res.render('template')
+      res.json(results)
+    })
+    .catch(e => {
+      console.error(e);
+      res.send(e);
+    })
+  } else {
+    res.redirect('/')
+  }
+})
+
+// post /users/:id/messages/:messageid (message a user)
+router.post('/:id/messages/:messageid', (req, res) => {
+  if (req.session.user_id) {
+    const senderId = req.session.user_id;
+    const recId = req.params.messageid
+    sendMessage(req.body, senderId, recId)
+    .then((message) => {
+      res.send(message);
+    })
+    .catch(e => {
+      console.error(e);
+      res.send(e);
+    })
+  } else {
+    res.redirect('/');
+  }
+})
 
 module.exports = router;
