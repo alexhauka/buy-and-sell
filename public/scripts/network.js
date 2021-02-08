d2c53cf4da15b782b937a2070d66387335e0f15e
chanoonna <chanoonna@gmail.com>
2021-02-08 23:56:58 +0000

    add: submitItem(), getSearchListings(). Both return jquery ajax which POSTs forms to the server

--- public/scripts/network.js
+++ public/scripts/network.js
@@ -1,16 +1,32 @@
 const getAllListings = function(params) {
   let url = `/items`;
+
   if (params) {
     url += "?" + params;
   }
   return $.ajax({
     url,
   });
-}
+};
 
 const getItem = function(id) {
-  let url = `/items/${id}`;
   return $.ajax({
-    url,
+    url: `/items/${id}`,
   });
-}
+};
+
+const submitItem = function(data) {
+  return $.ajax({
+    method: 'POST',
+    url: '/items/new',
+    data,
+  });
+};
+
+const getSearchListings = function(data) {
+  return $.ajax({
+    method: 'POST',
+    url: '/search',
+    data,
+  });
+};
