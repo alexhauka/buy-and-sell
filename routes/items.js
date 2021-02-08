af93e3c1a7e976e57cd456ebcca5c8ed8ba79a54
alexhauka <alexhauka@gmail.com>
2021-02-08 00:46:55 +0000

    add post route for adding comment to an item

--- routes/items.js
+++ routes/items.js
@@ -3,7 +3,12 @@
 // routes for items
 const express = require('express');
 const router  = express.Router();
-const { getItems, getItemById, addItem } = require('../lib/item-queries');
+const {
+  getItems,
+  getItemById,
+  addItem,
+  addComment
+} = require('../lib/item-queries');
 
 // get /items
 router.get('/', (req, res) => {
@@ -27,19 +32,41 @@ router.get('/:id', (req, res) => {
 router.get('/new', (req, res) => {
   res.send('post a new item page')
   // res.render('whateverTheTemplateIs')
-})
+});
 
 //post items/new
 router.post('/new', (req, res) => {
-  const userId = req.session.userId;
-  addItem({...req.body, user_id: userId})
-  .then(item => {
-    res.send(item);
-  })
-  .catch(e => {
-    console.error(e);
-    res.send(e);
-  });
-})
+  if (req.session.user_id) {
+    const userId = req.session.userId;
+    addItem({...req.body, user_id: userId})
+    .then(item => {
+      res.send(item);
+    })
+    .catch(e => {
+      console.error(e);
+      res.send(e);
+    })
+  } else {
+    res.redirect('/');
+  };
+});
+
+//post items/:id  (comment on item)
+router.post('/:id', (req, res) => {
+  if (req.session.user_id) {
+    const itemId = req.params.id;
+    const userId = req.session.user_id;
+    addComment(req.body, itemId, userId)
+    .then((comment) => {
+      res.send(comment);
+    })
+    .catch(e => {
+      console.error(e);
+      res.send(e);
+    })
+  } else {
+    res.redirect('/');
+  };
+});
 
 module.exports = router;
