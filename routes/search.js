67875d0fa3347818a6e8370546c73d8289cba592
chanoonna <chanoonna@gmail.com>
2021-02-10 05:27:07 +0000

    change routes/search.js
    Now search routes functions as it is required.

--- routes/search.js
+++ routes/search.js
@@ -5,14 +5,10 @@ const express = require('express');
 const router  = express.Router();
 const searchItems = require('../lib/search-queries');
 
-//get /search
-router.get('/', (req, res) => {
-  res.send('search page');
-});
-
 //post /search
-router.post('/', (req, res) => {
-  searchItems(req.body)
+router.get('/', (req, res) => {
+  console.log(req.query);
+  searchItems(req.query)
   .then((results) => {
     // res.render('template')
     res.json(results)
