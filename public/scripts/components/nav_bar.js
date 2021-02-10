d57d2342ccc65c6186a46af298c93e813a8e2987
James Richardson <jamesrichardson604@gmail.com>
2021-02-10 00:10:05 +0000

    added nav bar

--- public/scripts/components/nav_bar.js
+++ public/scripts/components/nav_bar.js
@@ -1,28 +1,29 @@
 $(() => {
 
-  const loadNavBar = function () {
-    const navBar = `
-    <nav>
-    <div id ="logo-links-and-name">
-      <div id ="logo">
-        <img src ="https://64.media.tumblr.com/294fcb5cb69d7bb052b94d82f686b7c8/tumblr_pmrd24Nz3a1qkm0xd_400.jpg" alt = "logo">
-      </div>
-      <div id ="links-and-name">
-        <div id="company-name">
-          <h3>
-            Buy And Sell !
-          </h3>
+  const loadNavBar = function (user) {
+    let navBar;
+    if (!user) {
+      navBar = `
+        <div id ="logo-links-and-name">
+          <div id ="logo">
+            <img src ="https://64.media.tumblr.com/294fcb5cb69d7bb052b94d82f686b7c8/tumblr_pmrd24Nz3a1qkm0xd_400.jpg" alt = "logo">
+          </div>
+          <div id ="links-and-name">
+            <div id="company-name">
+              <h3>
+                Buy And Sell !
+              </h3>
+            </div>
+            <div id ="links">
+              <ol>
+                <a href = "/login/1" id = "login">Login</a>
+                <a href = "">Register</a>
+                <a id ="search-bar-link">Quick Search</a>
+                <a href = "/search">Advanced Search</a>
+              </ol>
+            </div>
+          </div>
         </div>
-        <div id ="links">
-          <ol>
-            <a href = "">Login</a>
-            <a href = "">New Post</a>
-            <a id ="search-bar-link">Quick Search</a>
-            <a href = "">Advanced Search</a>
-          </ol>
-        </div>
-      </div>
-    </div>
         <div id ="search-bar">
             <button type ="button" id="cancel-button">x</button>
           <div>
@@ -33,12 +34,58 @@ $(() => {
             </form>
           </div>
         </div>
-    </nav>
-  `
-  $('body').append(navBar)
+      `
+    } else {
+      navBar = `
+        <div id ="logo-links-and-name">
+          <div id ="logo">
+            <img src ="https://64.media.tumblr.com/294fcb5cb69d7bb052b94d82f686b7c8/tumblr_pmrd24Nz3a1qkm0xd_400.jpg" alt = "logo">
+          </div>
+          <div id ="links-and-name">
+            <div id="company-name">
+              <h3>
+                Buy And Sell !
+              </h3>
+            </div>
+            <div id ="links">
+              <ol>
+                <a href = "/items">New Post</a>
+                <a href = "/users/:id/messages" id = "messages">Messages</a>
+                <a id ="search-bar-link">Quick Search</a>
+                <a href = "/search">Advanced Search</a>
+                <a href = "/logout">Logout</a>
+              </ol>
+            </div>
+          </div>
+        </div>
+        <div id ="search-bar">
+            <button type ="button" id="cancel-button">x</button>
+          <div>
+            <form>
+              <label for="search-text">Search by item:</label>
+              <input type="text" id="search-text" name="search-text">
+              <button type ="submit" id="search-button">Search</button>
+            </form>
+          </div>
+        </div>
+      `
+    }
+    $('nav').append(navBar)
   }
 
-  loadNavBar()
+  let userID;
+
+  $.get('users/:id/messages')
+  .then(msgObj => {
+    if (msgObj[0].id) {
+      userID = msgObj[0].id
+      loadNavBar(userID)
+    } else {
+      userID = null
+      loadNavBar(userID)
+    }
+  });
+
   $('#search-bar').hide()
 
   $('#search-bar-link').on('click', function () {
@@ -47,6 +94,18 @@ $(() => {
 
   $('#cancel-button').on('click', function () {
     $('#search-bar').slideUp()
+  })
+
+  $('#search-button').on('submit', function () {
+    //add a post request to /search on submit
+  })
+
+  $('#messages').on('click', function () {
+    // getMessageByUser(user)
+    // .then(msgObj => {
+    //   console.log(msgObj)
+    //   loadMessages(msgObj[0])
+    // })
   })
 
 });
