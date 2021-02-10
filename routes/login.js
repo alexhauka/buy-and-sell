7e366a3824d8b0cc8f0b937ddd733123000db9de
chanoonna <chanoonna@gmail.com>
2021-02-10 05:24:13 +0000

    change routes/login.js
    A little change to loing.js
    GET '/login' now send back user data with logged in ID.

--- routes/login.js
+++ routes/login.js
@@ -4,6 +4,11 @@
 const express = require('express');
 const router  = express.Router();
 
+//  Just copied fomr users.js for simple login featuer and fetching user information to the client.
+const {
+  getUserById,
+} = require('../lib/user-queries');
+
 
 //get /login (may not need this)
 router.get('/', (req, res) => {
@@ -13,8 +18,11 @@ router.get('/', (req, res) => {
 //get /login/:id
 router.get('/:id', (req, res) => {
   req.session.user_id = req.params.id;
-  res.redirect('/');
-  console.log(req.session.user_id)
+  // This is changed from redirecting to the main to sending user data.
+  getUserById(req.params.id)
+  .then((user) => {
+    res.json(user)
+  });
 });
 
 module.exports = router;
