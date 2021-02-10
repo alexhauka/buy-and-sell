23d45d99817f5ee86e91abba7718e415beea2d1f
chanoonna <chanoonna@gmail.com>
2021-02-10 05:16:01 +0000

    add item.js
    item.js renders a single item page with more detail.
    It will automatically load comments and a textbox for new comment.

--- public/scripts/components/item.js
+++ public/scripts/components/item.js
@@ -1,23 +1,27 @@
 $(() => {
-  const createItem = function(item) {
+  const createDetailedItemElem = function(item) {
     return `
-      <article class="item-detail">
+      <article class="item-detail" id="item_id_${item.id}">
         <section class="item-image">
           <imb src="${item.img_url}" alt="None"></imb>
         </section>
         <section class="item-name">
           <h4>${item.name}</h4>
-          <span>${item.date_posted}</span>
+          <span>${item.date_posted.slice(0, 19).replace('T', ' ')}</span>
           <span>From User: ${item.user_id}</span>
         </section>
         <section class="item-detail">
           <p>Description:<br>${item.description}</p>
           <div>
-            Price: ${item.price}
+            Price: ${(item.price / 100).toFixed(2)}
           </div>
           <div>
             SOLD: ${item.is_sold}
           </div>
+        </section>
+        <section class="new-comment">
+        </section>
+        <section class="comments">
         </section>
       </article>
     `;
@@ -33,12 +37,17 @@ $(() => {
 
   window.item = {};
 
-  const addItem = function(data) {
-    $item.empty();
-    const item = createItem(data);
-    $item.append(item);
+  const showItem = function(data) {
+    return getComments(data.id)
+      .then((commentsData) => {
+        comments.showComments(commentsData);
+        const item = createDetailedItemElem(data);
+        $item.empty();
+        $item.append(item);
+        $item.find('.new-comment').append($comment);
+        $item.find('.comments').append($comments);
+      })
   }
 
-  window.item.addItem = addItem;
-
+  window.item.showItem = showItem;
 });
